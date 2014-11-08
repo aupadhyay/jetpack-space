048857cafee3fc53c50718484c71856f7e1803a9
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2014-11-08 13:57:48 -0800

    test

---
+++ 1.0/menuScreen.lua
@@ -0,0 +1,23 @@
+_W = display.contentWidth
+_H = display.contentHeight
+
+local storyboard = require("storyboard")
+
+local menuScreen = storyboard.newScene("menuScreen")
+
+local bg
+
+
+function menuScreen:enterScene(e)
+	print("create")
+	bg = display.newImageRect( "Icon.png",100,100)
+	bg.x = 0
+	bg.y = 0
+
+end
+
+
+
+menuScreen:addEventListener( "enterScene", menuScreen )
+
+return menuScreen
