48688367e79e664c3479ed2b2f0c9d2309804e49
Cyrus Vachha <cyrus.vachha@yahoo.com>
2014-11-08 15:43:49 -0800

    Updated Bugs in gameScreen

--- 1.0/gameScreen.lua
+++ 1.0/gameScreen.lua
@@ -1,10 +1,10 @@
 
 --initiating libraries 
-local storyboard = require "storyboard"
+local storyboard = require ("storyboard")
 
 
 
-local gameScreen = storyboard.newScene("gaemScreen")
+local gameScreen = storyboard.newScene("gameScreen")
 
 --basic variables
 _W = display.contentWidth
@@ -32,12 +32,34 @@ function gameScreen:createScene(e)
     bg.x = _W/2
     bg.y = _H/2
     
-    startButton = display.newImageRect("Icon-60", 270,50)--insert proper image
+    startButton = display.newImageRect("Icon-60.png", 270,50)--insert proper image
     startButton.anchorX = 0.5
     startButton.anchorY = 0.5
     startButton.x = _W/2
     startButton.y = _H/2
-    startButton:addEventListener(startGame,"touch")
+    startButton:addEventListener("touch", startGame)
+
+    
+
+    --Game Playable Elements
+
+    leftWall = display.newImageRect("Icon-76.png",45,_H)--insert proper image
+    leftWall.anchorX = 0.5
+    leftWall.anchorY = 0.5
+    leftWall.x = 22
+    leftWall.y = _H/2
+
+    rightWall = display.newImageRect("Icon-76.png",45,_H)--insert proper image
+    rightWall.anchorX = 0.5
+    rightWall.anchorY = 0.5
+    rightWall.x = 298
+    rightWall.y = _H/2
+
+    player = display.newImageRect("Icon-60.png", 60,50)--insert proper image
+    player.anchorX = 0.5
+    player.anchorY = 0.5
+    player.x = _W/2
+    player.y = _H-30
 
     --Score Chart
     scoreChart = display.newImageRect("Icon-72.png",_W,70)--insert proper image
@@ -46,11 +68,11 @@ function gameScreen:createScene(e)
     scoreChart.x = _W/2
     scoreChart.y = 35
     
-    scoreText = display.newText("Score:",_W/2,460,30,native.systemFont)--insert proper font
-    scoreText:setColor(255,124,46)
+    scoreText = display.newText("Score:",_W/2-60,20,native.systemFont, 30)--insert proper font
+    --scoreText:setFillColor(255/255,124/255,46/255)
     
-    scoreNumText = display.newText(scoreNum,260,30,36,native.systemFont)--insert proper font
-    scoreNumText:setColor( 245, 53, 45 )
+    scoreNumText = display.newText("scoreNum",260,30,native.systemFont, 36)--insert proper font and scorenum
+    --scoreNumText:setFillColor( 245/255,53/255, 45/255 )
     
     pauseButton = display.newImageRect("Icon-60.png",30,30)--insert proper image
     pauseButton.anchorX = 0.5
@@ -60,25 +82,7 @@ function gameScreen:createScene(e)
     
     --------------
 
-    --Game Playable Elements
 
-    leftWall = display.newImageRect("Icon-76",45,_H)--insert proper image
-    leftWall.anchorX = 0.5
-    leftWall.anchorY = 0.5
-    leftWall.x = 22
-    leftWall.y = _H/2
-
-    rightWall = display.newImageRect("Icon-76",45,_H)--insert proper image
-    rightWall.anchorX = 0.5
-    rightWall.anchorY = 0.5
-    rightWall.x = 298
-    rightWall.y = _H/2
-
-    player = display.newImageRect("Icon-60", 60,50)--insert proper image
-    player.anchorX = 0.5
-    player.anchorY = 0.5
-    player.x = _W/2
-    player.y = _H-30
 end
 
 
@@ -92,7 +96,7 @@ end
 
 
 
-gameScreen:addEventListener("creatScene",gameScreen)
+gameScreen:addEventListener("createScene",gameScreen)
 
 
 return gameScreen
