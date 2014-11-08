fc935dfb55d27695d4f8096c8cb3e4f5de40ce8e
Cyrus Vachha <cyrus.vachha@yahoo.com>
2014-11-08 15:31:23 -0800

    Fixed Conflict

--- 1.0/gameScreen.lua
+++ 1.0/gameScreen.lua
@@ -3,7 +3,10 @@
 local storyboard = require "storyboard"
 
 
---basic varibles
+
+local gameScreen = storyboard.newScene("gaemScreen")
+
+--basic variables
 _W = display.contentWidth
 _H = display.contentHeight
 
@@ -14,35 +17,82 @@ local scoreChart
 local scoreText
 local scoreNumText
 local pauseButton
+local leftWall
+local rightWall
+local player
+local startButton
+local startGame = {}
 
 function gameScreen:createScene(e)
-    
-    bg = display.newImageRect("Icon-60.png",320,480)--insert proper image
+--Game Elements
+
+    bg = display.newImageRect("Icon-60.png",_W,_H)--insert proper image
     bg.anchorX = 0.5
     bg.anchorY = 0.5
     bg.x = _W/2
     bg.y = _H/2
     
-    
-    scoreChart = display.newImageRect("Icon-72.png",320,50)--insert proper image
+    startButton = display.newImageRect("Icon-60", 270,50)--insert proper image
+    startButton.anchorX = 0.5
+    startButton.anchorY = 0.5
+    startButton.x = _W/2
+    startButton.y = _H/2
+    startButton:addEventListener(startGame,"touch")
+
+    --Score Chart
+    scoreChart = display.newImageRect("Icon-72.png",_W,70)--insert proper image
     scoreChart.anchorX = 0.5
     scoreChart.anchorY = 0.5
     scoreChart.x = _W/2
-    scoreChart.y = _H-25
+    scoreChart.y = 35
     
-    scoreText = display.newText("Score:",_W/2,_H-30,36,native.systemFont)
+    scoreText = display.newText("Score:",_W/2,460,30,native.systemFont)--insert proper font
     scoreText:setColor(255,124,46)
     
-    scoreNumText = display.newText(scoreNum,_W/2+50,_H-30,36,native.systemFont)
+    scoreNumText = display.newText(scoreNum,260,30,36,native.systemFont)--insert proper font
     scoreNumText:setColor( 245, 53, 45 )
     
+    pauseButton = display.newImageRect("Icon-60.png",30,30)--insert proper image
+    pauseButton.anchorX = 0.5
+    pauseButton.anchorY = 0.5
+    pauseButton.x = 35
+    pauseButton.y = 35
+    
+    --------------
+
+    --Game Playable Elements
+
+    leftWall = display.newImageRect("Icon-76",45,_H)--insert proper image
+    leftWall.anchorX = 0.5
+    leftWall.anchorY = 0.5
+    leftWall.x = 22
+    leftWall.y = _H/2
+
+    rightWall = display.newImageRect("Icon-76",45,_H)--insert proper image
+    rightWall.anchorX = 0.5
+    rightWall.anchorY = 0.5
+    rightWall.x = 298
+    rightWall.y = _H/2
+
+    player = display.newImageRect("Icon-60", 60,50)--insert proper image
+    player.anchorX = 0.5
+    player.anchorY = 0.5
+    player.x = _W/2
+    player.y = _H-30
 end
 
 
+--GAME FUNCTIONS
+    
+function startGame(e)
+    
+    print "The Game has Started!"
+end   
 
 
 
 
+gameScreen:addEventListener("creatScene",gameScreen)
 
 
 return gameScreen
