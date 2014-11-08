943c561972543c1af3bc101f42227ad30885bfb1
Rayyaan-Mustafa <sonicshadow6@gmail.com>
2014-11-08 14:38:02 -0800

    Rayyaan's Changes
    SettingsScreen

---
+++ 1.0/settingsScreen.lua
@@ -0,0 +1,19 @@
+local storyboard = require("storyboard")
+
+local settingsScreen = storyboard.newScene("settingsScreen")
+local bg = ("Icon.png")
+
+
+function settingsScreen:enterScene(e)
+print ("SETTINGS!")
+local text = display.newText("SETTINGS!", 100,100 native.systemFont, 24)
+end
+
+
+
+
+settingsScreen:addEventListener("enterScene", settingsScreen)
+
+return settingsScreen
+end
+
