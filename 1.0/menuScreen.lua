a2db858513ba60c510c7da17c52da38db9cb7559
Rohan Jayaprakash <jayaprakash.rohan@gmail.com>
2014-11-08 14:36:50 -0800

    Added settings button

--- 1.0/menuScreen.lua
+++ 1.0/menuScreen.lua
@@ -7,13 +7,20 @@ local menuScreen = storyboard.newScene("menuScreen")
 
 local bg
 
+local changeScreen = {}
 
 function menuScreen:enterScene(e)
 	bg = display.newImageRect( "Icon.png",100,100)
 	bg.x = 0
 	bg.y = 0
+	bg:addEventListener( "tap", changeScreen )
+end
+
+function changeScreen(e)
+	storyboard.gotoScene("settingsScreen")
 end
 
 menuScreen:addEventListener( "enterScene", menuScreen )
 
 return menuScreen
+
