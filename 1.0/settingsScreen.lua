65d18d6e3b8a64a4426e6ac7468202914b22ef3a
Rohan Jayaprakash <jayaprakash.rohan@gmail.com>
2014-11-08 20:14:25 -0800

    Rohan
    Re-doing the settingScreen.lua file

--- 1.0/settingsScreen.lua
+++ 1.0/settingsScreen.lua
@@ -13,13 +13,15 @@ local instructionPage
 
 function settingsScreen:enterScene(e)
 	print ("SETTINGS!")
-	text = display.newText("SETTINGS!", 160,100, "COCOGOOSE", 24)
-	text:setFillColor(50/255,130/255,240/255)
+	text = display.newText("SETTINGS", 160,100, "game over", 60)
+	text:setFillColor(126/255,86/255,167/255)
 	settingsGroup:insert(text)
 
-	bg = display.newImageRect( "Icon-60.png",100,100)
-	bg.x = 270
-	bg.y = 475
+	bg = display.newImageRect( "images/help.png", 320 , 80)
+	bg.anchorX = 1
+	bg.anchorY = 1
+	bg.x = 320
+	bg.y = 568
 	settingsGroup:insert(bg)
 	bg:addEventListener( "tap", addHelpScreen )
 
@@ -33,9 +35,9 @@ function addHelpScreen(e)
 
 	
 
-	helpTitle = display.newText("INSTRUCTIONS", 160,100, "COCOGOOSE", 24)
-	helpTitle:setFillColor(200/255,0/255,0/255)
-	instructionPage = display.newText("Tilt your device left or right to \n \n guide General Jump through \n \n the asteroid belt as he passes \n \n between obstacles. Avoid the \n \n space-rocks along the edges \n \n of General's path.", 100, 100,  "Comic Sans MS", 20 )
+	helpTitle = display.newText("INSTRUCTIONS", 160,100, "game over", 60)
+	helpTitle:setFillColor(126/255,86/255,167/255)
+	instructionPage = display.newText("Tilt your device left or right to \n \n guide General Jump through \n \n the asteroid belt as he passes \n \n between obstacles. Avoid the \n \n space-rocks along the edges \n \n of General's path.", 100, 100,  "Game Over", 50 )
 	instructionPage.anchorX = 0.5
 	instructionPage.anchorY = 0.5
 	instructionPage.x = _W/2 
