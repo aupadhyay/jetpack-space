c758cd77a9c7ee64fd8858c67c0916dfa2d3f3c4
Cyrus Vachha <cyrus.vachha@yahoo.com>
2014-11-08 17:05:43 -0800

    Cyrus's Major Game Update

--- 1.0/gameScreen.lua
+++ 1.0/gameScreen.lua
@@ -1,7 +1,7 @@
 
 --initiating libraries 
 local storyboard = require ("storyboard")
-
+local physics = require("physics")
 
 
 local gameScreen = storyboard.newScene("gameScreen")
@@ -17,49 +17,44 @@ local scoreChart
 local scoreText
 local scoreNumText
 local pauseButton
-local leftWall
-local rightWall
 local player
 local startButton
+local asteroid = {}
 local startGame = {}
+local spawnAsteroids = {}
+local spinAsteroids = {}
+local update = {}
 
 function gameScreen:createScene(e)
 --Game Elements
 
-    bg = display.newImageRect("Icon-60.png",_W,_H)--insert proper image
+    bg = display.newImageRect("images/backgroundJump.jpg",_W,_H)--insert proper image
     bg.anchorX = 0.5
     bg.anchorY = 0.5
     bg.x = _W/2
     bg.y = _H/2
     
-    startButton = display.newImageRect("Icon-60.png", 270,50)--insert proper image
-    startButton.anchorX = 0.5
-    startButton.anchorY = 0.5
-    startButton.x = _W/2
-    startButton.y = _H/2
-    startButton:addEventListener("touch", startGame)
-
+    
     
 
     --Game Playable Elements
 
-    leftWall = display.newImageRect("Icon-76.png",45,_H)--insert proper image
-    leftWall.anchorX = 0.5
-    leftWall.anchorY = 0.5
-    leftWall.x = 22
-    leftWall.y = _H/2
 
-    rightWall = display.newImageRect("Icon-76.png",45,_H)--insert proper image
-    rightWall.anchorX = 0.5
-    rightWall.anchorY = 0.5
-    rightWall.x = 298
-    rightWall.y = _H/2
 
     player = display.newImageRect("Icon-60.png", 60,50)--insert proper image
     player.anchorX = 0.5
     player.anchorY = 0.5
     player.x = _W/2
     player.y = _H-30
+
+    startButton = display.newImageRect("Icon-60.png", 270,50)--insert proper image
+    startButton.anchorX = 0.5
+    startButton.anchorY = 0.5
+    startButton.x = _W/2
+    startButton.y = _H/2
+    
+    startButton:addEventListener("touch", startGame)
+
 
     --Score Chart
     scoreChart = display.newImageRect("Icon-72.png",_W,70)--insert proper image
@@ -68,10 +63,10 @@ function gameScreen:createScene(e)
     scoreChart.x = _W/2
     scoreChart.y = 35
     
-    scoreText = display.newText("Score:",_W/2-60,20,native.systemFont, 30)--insert proper font
+    scoreText = display.newText("Score:",_W/2-70,15,native.systemFont, 40)--insert proper font
     --scoreText:setFillColor(255/255,124/255,46/255)
     
-    scoreNumText = display.newText("scoreNum",260,30,native.systemFont, 36)--insert proper font and scorenum
+    scoreNumText = display.newText("100",_W/2+60,17,native.systemFont, 45)--insert proper font and scoreNum
     --scoreNumText:setFillColor( 245/255,53/255, 45/255 )
     
     pauseButton = display.newImageRect("Icon-60.png",30,30)--insert proper image
@@ -87,16 +82,51 @@ end
 
 
 --GAME FUNCTIONS
-    
+
+
+
 function startGame(e)
     
     print "The Game has Started!"
+    startButton.isVisible = false
+    timer.performWithDelay( 250, spawnAsteriods, 1 )
+    timer.performWithDelay( 1500, spawnAsteriods, 10 )
+ --   timer.performWithDelay( 500, spinAsteroids, 100 )
+    timer.performWithDelay( 500, transelateAsteroids, 15 )
+
+    physics:start()
 end   
 
+function spawnAsteriods(e)
+    
+    --print "Spawning Asteroid"
+for i = 1,10,1 do
+    print (i)
+    asteroid[i] = display.newImageRect("Icon-60.png", 50,50)--insert proper image
+    asteroid[i].anchorX = 0.5
+    asteroid[i].anchorY = 0.5
+    asteroid[i].x = math.random(20, 250)
+    asteroid[i].y = -30
+    physics.addBody( asteroid[i],"static", { friction=0.5, bounce=2.3 } )
+end
+end
+--function spinAsteroids(e)
+  --  for i  = 1,10,1 do
+   -- asteroid[i].rotation = asteroid[i].rotation + 3
+--end
+--end
 
 
+function update()
+    for i  = 1,10,1 do
+        if(not(asteroid[i] == nil))then
+            asteroid[i].y = asteroid[i].y + 3
+        end
+    end
+end
 
 gameScreen:addEventListener("createScene",gameScreen)
+Runtime:addEventListener( "enterFrame", update )
 
 
 return gameScreen
