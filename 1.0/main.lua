912c7691bd59bd8b0f82b05183e93d6e61f06655
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-04-09 16:58:59 -0700

    Deleted 1.0 Folder

--- 1.0/main.lua
+++
@@ -1,7 +0,0 @@
-local storyboard = require("storyboard")
-
-display.setStatusBar( display.HiddenStatusBar )
-system.setIdleTimer(false)
-system.setAccelerometerInterval(40)
-storyboard.gotoScene("menuScreen")
-
