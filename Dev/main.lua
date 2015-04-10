9daf5fe31d50df799bfe0a321d1aec10e3fe1850
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-04-09 17:19:29 -0700

    Saving

--- Dev/main.lua
+++ Dev/main.lua
@@ -1,5 +1,5 @@
 local storyboard = require("storyboard")
-display.setStatusBar( display.HiddenStatusBar )
+display.setStatusBar( display.HiddenStatusBar)
 system.setIdleTimer(false)
 system.setAccelerometerInterval(40)
 storyboard.gotoScene("menuScreen")
